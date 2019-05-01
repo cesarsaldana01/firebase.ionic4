e098a2554a2da684202abe34ae80cb03436cf439
cesarsaldana01 <41275096+cesarsaldana01@users.noreply.github.com>
2019-04-30 19:21:13 -0600

    Update credentials.ts

--- src/app/credentials.ts
+++ src/app/credentials.ts
@@ -1,9 +1,9 @@
 // Initialize Firebase
 export const firebaseConfig = {
-    apiKey: 'AIzaSyBrBZBaT4f4y4OEXhdGyPPFU7qY-Vq9wyA', 
-    authDomain: 'event-manager-5b6c6.firebaseapp.com', 
-    databaseURL: 'https://event-manager-5b6c6.firebaseio.com', 
-    projectId: 'event-manager-5b6c6', 
-    storageBucket: 'event-manager-5b6c6.appspot.com', 
-    messagingSenderId: '84201416203',
+    apiKey: '', 
+    authDomain: '', 
+    databaseURL: '', 
+    projectId: '', 
+    storageBucket: '', 
+    messagingSenderId: '',
     };
